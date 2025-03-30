db77240d0d28e6208a4175eb6de31ba524ad1279
terran-zealot <vikinglaurasun@gmail.com>
2025-03-30 11:35:40 +0000

    Deploying to gh-pages from @ terran-zealot/goit-js-hw-10@279a6a5a5823cc3d274a878358c3b6e813382646 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/vendor-CAHRx0Mg.js";function n(o){const t=Math.floor(o/864e5),c=Math.floor(o%864e5/36e5),s=Math.floor(o%864e5%36e5/6e4),r=Math.floor(o%864e5%36e5%6e4/1e3);return{days:t,hours:c,minutes:s,seconds:r}}console.log(n(2e3));console.log(n(14e4));console.log(n(2414e4));
+//# sourceMappingURL=1-timer.js.map
