e3ba2c90f646bfa975aae370ec205d9fb79dc6d8
terran-zealot <vikinglaurasun@gmail.com>
2025-03-30 15:31:35 +0300

    fixing npm iziToast in html

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,7 @@
 // Описаний у документації
-// import iziToast from "izitoast";
+import iziToast from "izitoast";
 // Додатковий імпорт стилів
-// import "izitoast/dist/css/iziToast.min.css";
+import "izitoast/dist/css/iziToast.min.css";
 
 
 
